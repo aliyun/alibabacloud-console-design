942cc7829d4fd7adc4dd2d260eeccfbb74c4fbf0
FringeY <465964690@qq.com>
2020-07-03 00:32:02 +0800

    feat: errorCenter upgrade (#10)

--- packages/xconsole-error-center/src/index.js
+++ packages/xconsole-error-center/src/index.js
@@ -1,16 +1,26 @@
-import alertError from './alert-error'
-// import './index.less';
+import consume from './consume'
+import { ConsoleNeedLogin } from './const'
+
+const defaultErrorCodes = {
+  [ConsoleNeedLogin]: {
+    enable: false
+  }
+}
 
 export default ({
   errorCenter: {
     enable = false,
     errorCode,
     errorCodes,
+    include,
+    exclude
   } = {},
 }) => {
   if (process.env.NODE_ENV === 'development' && errorCode) {
     console.error('[XConsole error-center]', '使用 errorCodes 来替代 errorCode(已废弃) '); // eslint-disable-line no-console
   }
+
+  const lastErrorCodes = Object.assign({}, defaultErrorCodes, errorCodes || errorCode || {});
 
   return {
     onError(err) {
@@ -20,21 +30,13 @@ export default ({
         console.error('[XConsole error-center]', err, err.response); // eslint-disable-line no-console
       }
 
-      // 自定义 errorPrompt
-      // if (errorPrompt) {
-      //   return errorPrompt(err)
-      // }
-
-      return ErrorPrompt(err, {
-        errorConfig: errorCodes || errorCode || {},
-      })
+      consume(err, lastErrorCodes, include, exclude);
     },
   }
 };
 
+export const ErrorConsume = consume;
+
 export const ErrorPrompt = (err, { errorConfig = {} }) => {
-  const _alertError = alertError({
-    errorConfig,
-  })
-  return _alertError(err);
+  consume(err, errorConfig);
 };
