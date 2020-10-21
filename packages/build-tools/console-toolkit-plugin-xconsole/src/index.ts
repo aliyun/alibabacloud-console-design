3d6eb9f6bdc70a486055c1381e34b3750b3c1afe
liusi.xbw <liusi.xbw@alibaba-inc.com>
2020-10-21 18:23:07 +0800

    fix: update the user status

--- packages/build-tools/console-toolkit-plugin-xconsole/src/index.ts
+++ packages/build-tools/console-toolkit-plugin-xconsole/src/index.ts
@@ -4,7 +4,7 @@ import { GENERATOR_URL } from './constants';
 
 export default function (api: PluginAPI) {
   api.dispatchSync('registerGenerator', 'XConsole', {
-    path: resolve(__dirname, '../generator/index.js'),
+    path: resolve(__dirname, '../generator/xconsole/index.js'),
     namespace: 'XconsoleProject',
     type: 'git',
     url: GENERATOR_URL,
