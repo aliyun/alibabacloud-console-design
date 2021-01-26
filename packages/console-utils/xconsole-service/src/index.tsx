7b6b1149a7419cc11079a15c6bbb8f570082765e
liusi.xbw <liusi.xbw@alibaba-inc.com>
2021-01-26 12:02:36 +0800

    feat: add the sideeffect for optmize

--- packages/console-utils/xconsole-service/src/index.tsx
+++ packages/console-utils/xconsole-service/src/index.tsx
@@ -1,6 +1,5 @@
 export * from './hooks/useService';
 export { default as useService } from './hooks/useService';
-export { default as useList } from './hooks/useList';
 export { default as createService, request as defaultAxiosRequest } from './service';
 export { setGetRegionIdFn } from './utils/index';
 export { default as globalConfig } from './configuration/config';
