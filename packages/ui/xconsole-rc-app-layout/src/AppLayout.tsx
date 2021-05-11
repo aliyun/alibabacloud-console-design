6d338355e40c9bd139fb27dc5b331d15ba16a53e
liusi.xbw <liusi.xbw@alibaba-inc.com>
2021-05-11 11:45:37 +0800

    fix(app-layout): bug ui no change when sidebar props changed in app layout

--- packages/ui/xconsole-rc-app-layout/src/AppLayout.tsx
+++ packages/ui/xconsole-rc-app-layout/src/AppLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import isFunction from 'lodash/isFunction';
 import { withRouter } from 'dva/router';
 import { IProp, ISidebarConfig } from './types/index';
@@ -32,6 +32,16 @@ const XConsoleAppLayout: React.FunctionComponent<IProp> = (props: IProp) => {
       '[xconsole rc-app-layout] sidebar.js 中关于 defaultOpenKeys collapsedKeys invisiblePaths 的配置不再推荐使用，请在 appConfig.js 中配置 consoleMenu， 具体配置信息及字段说明请前往官网查看 【开发指南】 文档。'
     );
   }
+
+  useEffect(() => {
+    if (sidebar.title !== title) {
+      setTitle(sidebar.title);
+    }
+    if (JSON.stringify(sidebar.navs) !== JSON.stringify(navs)) {
+      setTitle(sidebar.title);
+    }
+    setNavs(sidebar.navs);
+  }, [JSON.stringify(sidebar)])
 
   const onNavTriggerClick = useCallback((fn: () => void) => {
     const cb = (e) => {
