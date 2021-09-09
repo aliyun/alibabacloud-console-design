70d9c00cce44ed895441be9e25dedeecfa2f787a
liusi.xbw <liusi.xbw@alibaba-inc.com>
2021-09-09 21:01:31 +0800

    fix: fix type error for xconsole-service

--- packages/console-utils/xconsole-service/src/index.tsx
+++ packages/console-utils/xconsole-service/src/index.tsx
@@ -1,10 +1,7 @@
-import { ApiType as IApiType, IOptions } from './types';
+export type { ApiType, IOptions as ServiceOptions } from './types';
 
 export * from './hooks/useService';
 export { default as useService } from './hooks/useService';
 export { default as createService, request as defaultAxiosRequest } from './service';
 export { setGetRegionIdFn } from './utils/index';
 export { default as globalConfig } from './configuration/config';
-
-export type ApiType = IApiType;
-export type ServiceOption = IOptions;
