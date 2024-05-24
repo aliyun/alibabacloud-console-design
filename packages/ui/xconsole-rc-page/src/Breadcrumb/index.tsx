9161405b7c7f4b42cc1e26babf25e683d8166c3c
yr247308 <yr247308@alibaba-inc.com>
2024-05-24 17:52:41 +0800

    feat: rc-page breadcrumb props add alfaExternal

--- packages/ui/xconsole-rc-page/src/Breadcrumb/index.tsx
+++ packages/ui/xconsole-rc-page/src/Breadcrumb/index.tsx
@@ -1,12 +1,16 @@
 import React, { ReactElement } from 'react';
 import { Breadcrumb } from '@alicloud/console-components-page';
 import BaseLink from '@alicloud/xconsole-rc-base-link';
-import { Link } from 'dva/router';
+import { Link } from 'react-router-dom';
 
 export interface IBreadcrumbItem extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   text: string;
   link?: string;
   to?: string;
+  /**
+   * 是否在渲染 a 标签时添加 data-alfa-external-router
+   */
+  alfaExternal?: boolean;
 }
 
 export interface IProps {
@@ -16,10 +20,10 @@ export interface IProps {
 const RcBreadcrumb = function ({ items = [] }: IProps): ReactElement {
   return (
     <Breadcrumb>
-      {items.map(({ text, to, href, ...restProps }) => (
+      {items.map(({ text, to, href, alfaExternal, ...restProps }) => (
         <Breadcrumb.Item key={`${text}-${to}`}>
           {
-            to ? <Link to={to} href={href} {...restProps}>{text}</Link> :
+            to ? <Link to={to} href={href} {...restProps} data-alfa-external-router={alfaExternal || undefined}>{text}</Link> :
               href ? <BaseLink href={href} {...restProps}>{text}</BaseLink> : text
           }
         </Breadcrumb.Item>
